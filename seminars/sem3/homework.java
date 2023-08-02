9dedae2ab3813adb036e1ad54783df0cc9c3662b
DenitKO <motovdenis@mail.ru>
2023-08-03 00:19:08 +0300

    Finished HomeWork of 3 seminar

--- seminars/sem3/homework.java
+++ seminars/sem3/homework.java
@@ -1,7 +1,44 @@
 package seminars.sem3;
 
+import java.util.Arrays;
+
 public class homework {
     public static void main(String[] args) {
-        
+        long finish = HowManySteps(22,2022,11,10);
+        if ((finish==-1)||(finish==0)){
+            System.out.println("Нет решения");
+        } else{
+            System.out.println(finish);
+        }
+    }
+
+    public static long HowManySteps(int startPos, int endPos, int multyplyNum, int plusNum){
+        long howManyWays = -1;
+        int LengthOfTable = startPos>endPos ? startPos+1 : endPos+1;
+        long[] table = new long[LengthOfTable];
+        int steps = 0;
+        Arrays.fill(table, 0);
+        table[startPos]=1;
+        for (int i = startPos+1; i < endPos+1 ; i++) {
+            if (i-plusNum>=startPos){
+                if (i/multyplyNum>=startPos) {
+                    if ((i%multyplyNum==0)){
+                        table[i]=table[i-plusNum]+table[i/multyplyNum];
+                        steps++;
+                    } else {
+                        table[i]=table[i-plusNum];
+                        steps++;
+                    }
+                } else {
+                    table[i]=table[i-plusNum];
+                    steps++;
+                }
+            }
+        }
+        if (steps==0) return -1;
+        else{
+            howManyWays = table[LengthOfTable-1];
+            return howManyWays;
+        }
     }
 }
